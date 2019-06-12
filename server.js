1cb154a461d88e3a107fb0cea96e4877e58bc9a3
Lazar-Mihai <mihai2812@yahoo.com>
2019-06-12 03:49:21 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -51,6 +51,9 @@ function goToFile(filePath, request, response) {
 function goToPage(page, request, response) {
     var filePath = "./page/home.html"
     switch (page) {
+        case "fbpage":
+            filePath = "./page/fbPage.html"
+            break;
         case "category":
             filePath = "./page/category.html"
             break;
@@ -295,6 +298,10 @@ http.createServer(function (request, response) {
 
     } else {
         switch (request.url.toLowerCase()) {
+            case "/fbpage":
+            case "/fbpage.html":
+                goToPage("fbpage", request, response);
+                break;
             case "/category":
             case "/category.html":
                 goToPage("category", request, response);
@@ -344,46 +351,7 @@ http.createServer(function (request, response) {
         }
     }
 
-    // var extname = String(path.extname(filePath)).toLowerCase();
-    // var mimeTypes = {
-    //     '.html': 'text/html',
-    //     '.js': 'text/javascript',
-    //     '.css': 'text/css',
-    //     '.json': 'application/json',
-    //     '.png': 'image/png',
-    //     '.jpg': 'image/jpg',
-    //     '.gif': 'image/gif',
-    //     '.wav': 'audio/wav',
-    //     '.mp4': 'video/mp4',
-    //     '.woff': 'application/font-woff',
-    //     '.ttf': 'application/font-ttf',
-    //     '.eot': 'application/vnd.ms-fontobject',
-    //     '.otf': 'application/font-otf',
-    //     '.svg': 'application/image/svg+xml',
-    //     '.wasm': 'application/wasm'
-    // };
-
-    // var contentType = mimeTypes[extname] || 'application/octet-stream';
-
-    // fs.readFile(filePath, function(error, content) {
-    //     if (error) {
-    //         if(error.code == 'ENOENT') {
-    //             fs.readFile('./404.html', function(error, content) {
-    //                 response.writeHead(200, { 'Content-Type': contentType });
-    //                 response.end(content, 'utf-8');
-    //             });
-    //         }
-    //         else {
-    //             response.writeHead(500);
-    //             response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-    //             response.end();
-    //         }
-    //     }
-    //     else {
-    //         response.writeHead(200, { 'Content-Type': contentType });
-    //         response.end(content, 'utf-8');
-    //     }
-    // });
+   
 
 }).listen(8125);
 console.log('Server running at http://127.0.0.1:8125/');
